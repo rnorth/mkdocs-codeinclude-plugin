77d27178cec84cea7a65e8ce26a248386ce8647c
Richard North <rich.north@gmail.com>
2018-09-30 19:37:52 +0100

    various code selection mechanisms

--- codeinclude/plugin.py
+++ codeinclude/plugin.py
@@ -2,23 +2,44 @@ from mkdocs.plugins import BasePlugin
 import re
 import os
 
+from codeinclude.resolver import select
 
+RE_SNIPPET = r'''(?x)
+    \[(?P<filename>[^\]]+)\]
+    \(
+    snippet[ \t]*
+    (lang=(?P<lang>[a-z0-9]+))?[ \t]*
+    (lines=(?P<lines>[0-9,\-]+))?[ \t]*
+    (from_token=(?P<from_token>[^\s]+))?[ \t]*
+    (to_token=(?P<to_token>[^\s]+))?[ \t]*
+    (block=(?P<block>[^\s]+))?[ \t]*
+    \)
+'''
 
 class CodeIncludePlugin(BasePlugin):
     def on_page_markdown(self, markdown, page, config,
-                          site_navigation=None, **kwargs):
+                         site_navigation=None, **kwargs):
         "Provide a hook for defining functions from an external module"
         print(page.file.abs_src_path)
 
         def repl(m):
-            filename = m.group(1)
+            print(m)
+            filename = m.group("filename")
+            lang = m.group("lang") or ""
+            lines = m.group("lines")
+            from_token = m.group("from_token")
+            to_token = m.group("to_token")
+            block = m.group("block")
 
             page_parent_dir = os.path.dirname(page.file.abs_src_path)
             import_path = os.path.join(page_parent_dir, filename)
-
+            print(filename)
+            print(import_path)
             with open(import_path) as f:
                 content = f.read()
 
-            return "<pre><code>" + content + "</code></pre>"
+            selected_content = select(content, lines=lines, block=block, from_token=from_token, to_token=to_token)
 
-        return re.sub(r'% codeinclude ([^\s]+) %', repl, markdown)
+            return str.format("\n\n```{}\n{}\n```\n\n", lang, selected_content)
+
+        return re.sub(RE_SNIPPET, repl, markdown)
---
+++ codeinclude/resolver.py
@@ -0,0 +1,82 @@
+import re
+
+
+def select(text, 
+           lines=None, 
+           from_token=None, to_token=None, 
+           block=None, 
+           lang=None):
+    
+    selected_lines = []
+
+    if lines:
+        for line_range in lines.split(","):
+            range_match = re.match(r'(\d+)-(\d+)', line_range)
+            if range_match:
+                start = int(range_match.group(1))
+                end = int(range_match.group(2))
+                for i in range(start, end + 1):
+                    selected_lines.append(i)
+            else:
+                selected_lines.append(int(line_range))
+    
+    if block:
+        i = 0
+        delim_count = 0
+        for line in text.splitlines():
+            i = i + 1
+            print(delim_count)
+            if block in line and delim_count <= 0:
+                print("starting")
+                delim_count = 0
+                delim_count += line.count("{")
+                
+            if delim_count > 0:
+                print(line)
+                selected_lines.append(i)
+
+            delim_count -= line.count("}")
+
+    if from_token and to_token:
+        i = 0
+        active = False
+        for line in text.splitlines():
+            i = i + 1
+            if not active and from_token in line:
+                active = True
+            
+            if active:
+                selected_lines.append(i)
+            
+            if active and to_token in line:
+                active = False
+
+    result = ""
+    source_lines = text.splitlines()
+
+    last_selected = 0
+    for i in sorted(selected_lines):
+        if i > (last_selected + 1):
+            result += "\n⋯\n\n"
+        result += source_lines[i - 1] + "\n"
+        last_selected = i
+
+    return result
+
+
+s = """
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+k
+l
+"""
+
+print(select(s, lines="3-7,1"))
