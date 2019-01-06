b215fb80c36805890d4b3fa3cc97bd5bfdd66fda
Richard North <rich.north@gmail.com>
2019-01-06 16:50:34 +0000

    Revert to multi-tab capable format

--- codeinclude/plugin.py
+++ codeinclude/plugin.py
@@ -6,13 +6,28 @@ import textwrap
 from mkdocs.plugins import BasePlugin
 from codeinclude.resolver import select
 
+RE_START = r"""(?x)
+    ^
+    (?P<leading_space>\s*)
+    <!--codeinclude-->
+    (?P<ignored_trailing_space>\s*)
+    $
+"""
+
+RE_END = r"""(?x)
+    ^
+    (?P<leading_space>\s*)
+    <!--/codeinclude-->
+    (?P<ignored_trailing_space>\s*)
+    $
+"""
+
 RE_SNIPPET = r"""(?x)
     ^
     (?P<leading_space>\s*)
-    (?P<marker><!--codeinclude-->)
-    (?P<ignored_space>\s*)
     \[(?P<title>[^\]]*)\]\((?P<filename>[^)]+)\)
     ([\t ]+(?P<params>.*))?
+    (?P<ignored_trailing_space>\s*)
     $
 """
 
@@ -37,35 +52,49 @@ class CodeIncludePlugin(BasePlugin):
     def on_page_markdown(self, markdown, page, config, site_navigation=None, **kwargs):
         "Provide a hook for defining functions from an external module"
 
+        active = False
         results = ""
         for line in markdown.splitlines():
+            boundary = False
 
-            # handle each line
-            snippet_match = re.match(RE_SNIPPET, line)
-            if snippet_match:
-                title = snippet_match.group("title")
-                filename = snippet_match.group("filename")
-                indent = snippet_match.group("leading_space")
-                raw_params = snippet_match.group("params")
+            # detect end
+            if active and re.match(RE_END, line):
+                active = False
+                boundary = True
 
-                if raw_params:
-                    params = dict(token.split(":") for token in shlex.split(raw_params))
-                    lines = params.get("lines", "")
-                    block = params.get("block", "")
-                    inside_block = params.get("inside_block", "")
-                else:
-                    lines = ""
-                    block = ""
-                    inside_block = ""
+            # handle each line of a codeinclude zone
+            if active:
+                snippet_match = re.match(RE_SNIPPET, line)
+                if snippet_match:
+                    title = snippet_match.group("title")
+                    filename = snippet_match.group("filename")
+                    indent = snippet_match.group("leading_space")
+                    raw_params = snippet_match.group("params")
 
-                code_block = get_substitute(
-                    page, title, filename, lines, block, inside_block
-                )
+                    if raw_params:
+                        params = dict(token.split(":") for token in shlex.split(raw_params))
+                        lines = params.get("lines", "")
+                        block = params.get("block", "")
+                        inside_block = params.get("inside_block", "")
+                    else:
+                        lines = ""
+                        block = ""
+                        inside_block = ""
 
-                # re-indent
-                code_block = re.sub("^", indent, code_block, flags=re.MULTILINE)
-                results += code_block
-            else:
+                    code_block = get_substitute(
+                        page, title, filename, lines, block, inside_block
+                    )
+                    # re-indent
+                    code_block = re.sub("^", indent, code_block, flags=re.MULTILINE)
+                    results += code_block
+
+            # detect start
+            if re.match(RE_START, line):
+                active = True
+                boundary = True
+
+            # outside a codeinclude zone and ignoring the boundaries
+            if not active and not boundary:
                 results += line + "\n"
 
         return results
--- tests/codeinclude/test_plugin.py
+++ tests/codeinclude/test_plugin.py
@@ -12,7 +12,21 @@ MARKDOWN_EXAMPLE = """
 # hello world
 
 some text before
-<!--codeinclude--> [foo](Foo.java)
+<!--codeinclude-->
+[foo](Foo.java)
+<!--/codeinclude-->
+and some text after
+
+"""
+
+MULTI_TAB_MARKDOWN_EXAMPLE = """
+# hello world
+
+some text before
+<!--codeinclude--> 
+[foo](Foo.java) 
+[bar](Bar.java)
+<!--/codeinclude-->
 and some text after
 
 """
@@ -22,6 +36,7 @@ c["site_url"] = "http://example.org/"
 
 PAGE_EXAMPLE = Page("", File(os.path.abspath("./fixture/text.md"), "/src", "/dest", False), c)
 
+
 class PluginTextCase(unittest.TestCase):
 
     def test_simple_case(self):
@@ -29,7 +44,7 @@ class PluginTextCase(unittest.TestCase):
         result = plugin.on_page_markdown(MARKDOWN_EXAMPLE, PAGE_EXAMPLE, dict())
 
         print(result)
-        self.assertEqual(result.strip(), textwrap.dedent("""
+        self.assertEqual(textwrap.dedent("""
                                   # hello world
                                   
                                   some text before
@@ -41,4 +56,32 @@ class PluginTextCase(unittest.TestCase):
                                   ```
                                   
                                   and some text after
-                                  """).strip())
+                                  """).strip(),
+                         result.strip())
+
+    def test_multi_tab_case(self):
+        plugin = CodeIncludePlugin()
+        result = plugin.on_page_markdown(MULTI_TAB_MARKDOWN_EXAMPLE, PAGE_EXAMPLE, dict())
+
+        print(result)
+        self.assertEqual(textwrap.dedent("""
+                                  # hello world
+
+                                  some text before
+
+                                  ```java tab=\"foo\"
+                                  public class Foo {
+
+                                  }
+                                  ```
+
+
+                                  ```java tab=\"bar\"
+                                  public class Bar {
+
+                                  }
+                                  ```
+
+                                  and some text after
+                                  """).strip(),
+                         result.strip())
