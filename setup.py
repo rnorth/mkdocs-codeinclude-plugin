500aa233703241a45dff86197dc52db3f0d15bd0
Richard North <rich.north@gmail.com>
2020-06-18 18:44:40 +0100

    Tidy up setup.py

--- setup.py
+++ setup.py
@@ -16,21 +16,19 @@ def read_file(fname):
 setup(
     name='mkdocs-codeinclude-plugin',
     version='0.0.1',
-    description="TODO",
+    description="A plugin to include code snippets into mkdocs pages",
     long_description=read_file('README.md'),
     long_description_content_type='text/markdown',
     keywords='mkdocs python markdown codeinclude',
     url='TODO',
     author='Richard North',
-    author_email='TODO',
-    license='Apache-2.0',
+    author_email='rich.north@gmail.com',
+    license='MIT',
     python_requires='>=3.6',
     install_requires=[
         'mkdocs>=0.17',
-        'mkdocs'
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
@@ -39,13 +37,10 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     packages=['codeinclude'],
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     entry_points={
         'mkdocs.plugins': [
             'codeinclude = codeinclude.plugin:CodeIncludePlugin'
         ]
-    },
-    test_suite="nose.collector",
-    tests_require=["nose"]
+    }
 )
